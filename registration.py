6e461ea9286f4c5111f61bbbca113777127c7222
Dominic <notdom@users.noreply.github.com>
2016-10-06 20:35:32 -0500

    completed

--- registration.py
+++ registration.py
@@ -1,3 +1,6 @@
 import requests
 
-api_token = "4f36c1b5089b07d3416239291874b956"
+info = {'token': '4f36c1b5089b07d3416239291874b956', 'github': 'https://github.com/notdom/code2040'}
+r = requests.post("http://challenge.code2040.org/api/register", json=info)
+
+print(r.status_code)
